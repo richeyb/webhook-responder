97aa67709a007f8dc04ffb52cfd313fa5518be9a
Brandon Richey <brandon.richey@greenhouse.io>
2016-11-18 09:48:16 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+
+const router = express.Router();
+
+const debug = (body, status) => {
+  console.log('!! NEW REQUEST !!')
+  console.log("\tReceived a request:", status);
+  console.log("\tReceived data:", body);
+};
+
+router.post('/:status', (req, res) => {
+  const body = req.body;
+  debug(body, req.params.status);
+  if (req.params.status === '200') {
+    res.json(body);
+  } else {
+    res.sendStatus(req.params.status);
+  }
+});
+router.post('/', (req, res) => {
+  const body = req.body;
+  debug(body, 200);
+  res.json(body);
+});
+app.use('/', router);
+
+app.listen(port);
+console.log(`Server is running on port ${port}`);
